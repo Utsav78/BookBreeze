9bb31a6a0dcef4fe2b12e089ec6abe111623e575
Utsav Budathoki <utsavbudathoki12@gmail.com>
2023-12-26 14:47:57 +1030

    dark theme success

--- app/src/main/java/com/example/bookbreeze/MainActivity.kt
+++ app/src/main/java/com/example/bookbreeze/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.bookbreeze
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var loginButton: Button
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loginButton = findViewById(R.id.login_button)
+        loginButton.setOnClickListener {
+            Toast.makeText(this, "Login is successful", Toast.LENGTH_SHORT).show()
+        }
     }
 }
